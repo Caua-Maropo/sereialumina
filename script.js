57c291cdaadebad5396589fc1882912d9bc67018
Caua-Maropo <cauamaropo@gmail.com>
2025-12-31 19:29:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const produtos = document.querySelectorAll('.produto');
 
 function filtrarProdutos(categoria) {
   produtos.forEach(produto => {
-    if (categoria === 'todos' || produto.dataset.categoria === categoria) {
+    if (categoria === 'todos' || produto.dataset.category === categoria) {
       produto.style.display = 'block';
     } else {
       produto.style.display = 'none';
@@ -13,17 +13,14 @@ function filtrarProdutos(categoria) {
 
 botoes.forEach(botao => {
   botao.addEventListener('click', () => {
-    // botão ativo
     botoes.forEach(b => b.classList.remove('ativo'));
     botao.classList.add('ativo');
 
-    // pega categoria do atributo
     const categoria = botao.dataset.category;
     filtrarProdutos(categoria);
   });
 });
 
-// Garantir que todos apareçam ao carregar
 document.addEventListener('DOMContentLoaded', () => {
   filtrarProdutos('todos');
 });
