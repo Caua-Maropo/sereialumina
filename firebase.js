f3952609cbc6c0e66f0914e7ca000db2250d8234
Caua-Maropo <cauamaropo@gmail.com>
2026-01-02 02:09:17 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBp_mqQKIps49q48ilqIsvvKle3I2Rjgag",
@@ -15,3 +16,5 @@ export const app = initializeApp(firebaseConfig);
 
 // 🔐 exporta auth também
 export const auth = getAuth(app);
+
+export const db = getFirestore(app);
