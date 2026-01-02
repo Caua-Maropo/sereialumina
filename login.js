d7c7a443f35ffcbe51c07580884c1b471901e18f
Caua-Maropo <cauamaropo@gmail.com>
2026-01-02 11:54:23 -0300

    Create login.js

---
+++ login.js
@@ -0,0 +1,22 @@
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { app } from "./firebase.js";
+
+const auth = getAuth(app);
+
+const form = document.getElementById('form-login');
+const erroLogin = document.getElementById('erro-login');
+
+form.addEventListener('submit', async e => {
+  e.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const senha = document.getElementById('senha').value;
+
+  try {
+    const cred = await signInWithEmailAndPassword(auth, email, senha);
+    // login bem-sucedido → redireciona para perfil
+    window.location.href = 'perfil.html';
+  } catch (error) {
+    erroLogin.textContent = 'Email ou senha inválidos.';
+  }
+});
