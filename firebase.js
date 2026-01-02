dbdd15937615ab44a4eb33c2c35029a7b6c87361
Caua-Maropo <cauamaropo@gmail.com>
2026-01-02 01:45:38 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,17 @@
-console.log('🔥 Firebase inicializado');
-
-
-// Imports via CDN (funciona no navegador)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// 🔑 Configuração do Firebase (a SUA, sem mudar nada)
 const firebaseConfig = {
   apiKey: "AIzaSyBp_mqQKIps49q48ilqIsvvKle3I2Rjgag",
   authDomain: "seria-lumina.firebaseapp.com",
   projectId: "seria-lumina",
-  storageBucket: "seria-lumina.firebasestorage.app",
+  storageBucket: "seria-lumina.appspot.com",
   messagingSenderId: "388959223244",
   appId: "1:388959223244:web:15f1f9cc98a052fd397169"
 };
 
-// Inicializa Firebase
-const app = initializeApp(firebaseConfig);
+// 🔥 inicializa o app
+export const app = initializeApp(firebaseConfig);
 
-// Serviços que vamos usar
+// 🔐 exporta auth também
 export const auth = getAuth(app);
-export const db = getFirestore(app);
