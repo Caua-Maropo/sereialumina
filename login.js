36c462fefe64c8b78f71ebd958c3c01dc022f7da
Caua-Maropo <cauamaropo@gmail.com>
2026-01-02 23:10:13 -0300

    Update login.js

--- login.js
+++ login.js
@@ -2,18 +2,20 @@ import { auth } from './firebase.js';
 import { signInWithEmailAndPassword } from
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-const btn = document.getElementById('btn-login');
-const msg = document.getElementById('login-msg');
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('btn-login');
+  const msg = document.getElementById('login-msg');
 
-btn.addEventListener('click', async () => {
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+  btn.addEventListener('click', async () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
-  try {
-    await signInWithEmailAndPassword(auth, email, password);
-    window.location.href = "admin.html";
-  } catch (error) {
-    msg.textContent = "Email ou senha inválidos";
-    msg.style.color = "red";
-  }
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      window.location.href = "admin.html";
+    } catch (error) {
+      msg.textContent = "Email ou senha inválidos";
+      msg.style.color = "red";
+    }
+  });
 });
