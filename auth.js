71258a912d5bc791d65b9109f7a0e5cdb8a7b674
Caua-Maropo <cauamaropo@gmail.com>
2026-01-03 18:56:08 -0300

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,27 @@
+import { getAuth, onAuthStateChanged, signOut } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const auth = getAuth();
+
+const userArea = document.getElementById("user-area");
+const userEmail = document.getElementById("user-email");
+const logoutBtn = document.getElementById("logout-btn");
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // USUÁRIO LOGADO
+    userArea.style.display = "flex";
+    userEmail.textContent = user.email;
+  } else {
+    // NÃO LOGADO
+    userArea.style.display = "none";
+  }
+});
+
+if (logoutBtn) {
+  logoutBtn.addEventListener("click", () => {
+    signOut(auth).then(() => {
+      window.location.href = "login.html";
+    });
+  });
+}
