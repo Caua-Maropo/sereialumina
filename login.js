49a8e741a0b8b6425588c9e5df2773569854a6c2
Caua-Maropo <cauamaropo@gmail.com>
2026-01-02 23:07:11 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,32 +1,19 @@
 import { auth } from './firebase.js';
-import { signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { signInWithEmailAndPassword } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-const formLogin = document.getElementById('form-login-admin');
-const erroLogin = document.getElementById('erro-login');
+const btn = document.getElementById('btn-login');
+const msg = document.getElementById('login-msg');
 
-formLogin.addEventListener('submit', (e) => {
-  e.preventDefault();
-
+btn.addEventListener('click', async () => {
   const email = document.getElementById('email').value;
-  const senha = document.getElementById('senha').value;
+  const password = document.getElementById('password').value;
 
-  signInWithEmailAndPassword(auth, email, senha)
-    .then(() => {
-      // Login OK → redireciona para perfil
-      window.location.href = 'perfil.html';
-    })
-    .catch((err) => {
-      console.error(err);
-      erroLogin.textContent = 'Email ou senha incorretos!';
-    });
-});
-
-// Protege a página se o usuário já estiver logado
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // Se o usuário já estiver logado e estiver no login.html, envia direto pro perfil
-    if (window.location.pathname.includes('login.html')) {
-      window.location.href = 'perfil.html';
-    }
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "admin.html";
+  } catch (error) {
+    msg.textContent = "Email ou senha inválidos";
+    msg.style.color = "red";
   }
 });
