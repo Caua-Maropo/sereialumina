b6e9253fc5a1a2cb4fb799b011f2ff7161b03c9f
Caua-Maropo <cauamaropo@gmail.com>
2026-01-20 03:27:21 -0300

    Update produto.js

--- assets/js/produto.js
+++ assets/js/produto.js
@@ -40,7 +40,6 @@ btnCarrinho.disabled = true
 // ================================
 // TAMANHOS + ESTOQUE
 // ================================
-let tamanhoSelecionado = null;
 
 const listaTamanhos = document.getElementById("lista-tamanhos");
 const estoqueInfo = document.getElementById("estoque-info");
